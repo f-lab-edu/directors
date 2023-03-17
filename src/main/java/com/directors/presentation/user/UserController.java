689ccfd88e6a7459bdf0970a080e7ef7c3747040
tilsong <cnsong0229@gmail.com>
2023-03-17 18:00:53 +0900

    [#4] 회원 가입 기능 구현 - Controller
    - Controller에 아이디 중복 체크 요청을 받아 응답하는 isDuplicateId 메서드 추가

--- src/main/java/com/directors/presentation/user/UserController.java
+++ src/main/java/com/directors/presentation/user/UserController.java
@@ -2,13 +2,12 @@ package com.directors.presentation.user;
 
 import com.directors.presentation.user.request.SignUpRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -20,5 +19,15 @@ public class UserController {
         log.info("회원 가입 완료");
 
         return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{id}/duplicated")
+    public ResponseEntity<HttpStatus> idCheck(
+            @PathVariable @NotBlank(message = "입력 값이 존재하지않습니다.")
+            @Size(min = 8, max = 20, message = "아이디의 길이가 8-20글자 사이로 입력되지 않았습니다.") String id
+    ) {
+        log.info("요청된 id: " + id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
