a938f1e98ce364f0b36d918edc2570ce5e9a07d7
tilsong <cnsong0229@gmail.com>
2023-05-09 10:41:04 +0900

    [#48] 채팅방 Repository 테스트 코드 작성 - 기존 사용하지 않는 코드 제거

--- src/main/java/com/directors/infrastructure/jpa/room/JpaRoomRepository.java
+++ src/main/java/com/directors/infrastructure/jpa/room/JpaRoomRepository.java
@@ -4,12 +4,9 @@ import com.directors.domain.room.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface JpaRoomRepository extends JpaRepository<Room, Long> {
     List<Room> findByDirectorId(String directorId);
 
     List<Room> findByQuestionerId(String questionerId);
-
-    Optional<Room> findByQuestionId(Long questionId);
 }
