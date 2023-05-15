47f830a1df3c22b66799542ed2e1803876e279d7
tilsong <cnsong0229@gmail.com>
2023-05-15 23:00:13 +0900

    [#52] 레디스 연결 관련 의존성 및 설정 추가
    - 추가된 의존성
      - spring-boot-starter-data-redis
      - com.fasterxml.jackson.datatype:jackson-datatype-jsr310

---
+++ src/main/java/com/directors/infrastructure/util/ObjectMapperUtils.java
@@ -0,0 +1,30 @@
+package com.directors.infrastructure.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+
+public class ObjectMapperUtils {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    static {
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    public static <T> T readValue(String value, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(value, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String writeValueAsString(Object value) {
+        try {
+            return objectMapper.writeValueAsString(value);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
