19aacd12059958e9d456c1171ce4b7c4ec6f37b4
tilsong <cnsong0229@gmail.com>
2023-05-16 15:55:06 +0900

    [#52] Redis pub/sub 연동
    -  채팅방에 대한 sse 연결이 현재 존재할 경우 redis를 통해 채팅을 전달하도록 코드 작성

--- src/main/java/com/directors/infrastructure/chat/LiveChatManagerImpl.java
+++ src/main/java/com/directors/infrastructure/chat/LiveChatManagerImpl.java
@@ -1,12 +1,20 @@
 package com.directors.infrastructure.chat;
 
+import com.directors.domain.chat.Chat;
 import com.directors.domain.chat.LiveChatManager;
-import lombok.RequiredArgsConstructor;
+import com.directors.infrastructure.util.ObjectMapperUtils;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,11 +23,26 @@ import java.util.Map;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class LiveChatManagerImpl implements LiveChatManager {
-    // TODO: 04.14 @Transaction 적용 여부 판단 필요
+public class LiveChatManagerImpl implements LiveChatManager, MessageListener {
     Map<Long, List<SseEmitter>> sseListMap = new HashMap<>();
+    private final RedisMessageListenerContainer listenerContainer;
 
+    private final RedisTemplate<String, Object> redisTemplate;
+    private Map<String, ChannelTopic> channels;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        String body = getBodyString(message.getBody());
+        Chat chat = ObjectMapperUtils.readValue(body, Chat.class);
+
+        publishChatToRoom(chat.getRoomId(), chat);
+    }
+
+    // redis listener add / list 0 -> remove
     public void addReceiver(Long roomId, SseEmitter sseEmitter) {
+        ChannelTopic channel = new ChannelTopic(String.valueOf(roomId));
+        listenerContainer.addMessageListener(this, channel);
+
         if (!sseListMap.containsKey(roomId)) {
             makeListAndAddEmitter(roomId, sseEmitter);
         } else {
@@ -27,11 +50,35 @@ public class LiveChatManagerImpl implements LiveChatManager {
         }
     }
 
-    public void sendMessage(Long roomId, String chatContent, String sendUserId) {
+    public void sendChat(Long roomId, String chatContent, LocalDateTime sendTime, String sendUserId)  {
         if (!sseListMap.containsKey(roomId)) {
             return;
         }
 
+        Chat chat = Chat.of(roomId, chatContent, sendUserId, sendTime);
+
+        redisTemplate.convertAndSend(String.valueOf(roomId), ObjectMapperUtils.writeValueAsString(chat));
+    }
+
+    public void removeReceiver(Long roomId, SseEmitter sseEmitter) {
+        if (!sseListMap.containsKey(roomId)) {
+            return;
+        }
+
+        List<SseEmitter> emitters = sseListMap.get(roomId);
+        emitters.remove(sseEmitter);
+
+
+
+        log.info("roomId " + roomId + " | 1 User left. currentUser =>" + emitters.size());
+    }
+
+    private String getBodyString(byte[] body) {
+        String newBody = new String(body).replace("\\", "");
+        return newBody.substring(1, newBody.length() - 1);
+    }
+
+    private void publishChatToRoom(Long roomId, Chat chat) {
         List<SseEmitter> emitterList = sseListMap.get(roomId);
 
         if (emitterList.isEmpty()) {
@@ -40,23 +87,17 @@ public class LiveChatManagerImpl implements LiveChatManager {
 
         try {
             for (SseEmitter sseEmitter : emitterList) {
-                sseEmitter.send(chatContent); // data type text 필요. default => MediaType.APPLICATION_JSON
+                sseEmitter.send(chat); // data type text 필요. default => MediaType.APPLICATION_JSON
             }
         } catch (IOException e) {
             log.warn("Failed to send chat via SSE. roomId: " + roomId);
         }
     }
 
-    public void removeReceiver(Long roomId, SseEmitter sseEmitter) {
-        List<SseEmitter> emitters = sseListMap.get(roomId);
-        emitters.remove(sseEmitter);
-
-        log.info("roomId " + roomId + " | 1 User left. currentUser =>" + emitters.size());
-    }
-
     private void makeListAndAddEmitter(Long roomId, SseEmitter sseEmitter) {
         List<SseEmitter> emitterList = new ArrayList<>();
         emitterList.add(sseEmitter);
+
         sseListMap.put(roomId, emitterList);
     }
 
